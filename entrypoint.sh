b7169c881459c149c0505aea827de6a4061c552c
Dmitry <dmitry.parkhomenko@motorsport.com>
2019-12-30 11:44:07 +0200

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -128,14 +128,14 @@ function pushWithSnapshot() {
   local LAST_TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g" | sed -e "s/refs\/tags\///g")
   local LAST_VER=$(git describe --tags $(git rev-list --tags --max-count=1))
   local COMMITS_AHEAD=$(git rev-list ${LAST_VER}.. --count)
-    if ${COMMITS_AHEAD}=0; then
-     SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}"
-    fi;
-    if ${LAST_TAG}=${LAST_VER}; then
-     SNAPSHOT_TAG="${LAST_VER}"
-    fi;
   local SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}.${COMMITS_AHEAD}"
   local SHA_DOCKER_NAME="${INPUT_NAME}:${SNAPSHOT_TAG}"
+  if ${COMMITS_AHEAD}=0; then
+    SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}"
+  fi;
+  if ${LAST_TAG}=${LAST_VER}; then
+    SNAPSHOT_TAG="${LAST_VER}"
+  fi;
   docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} -t ${DOCKERNAME} -t ${SHA_DOCKER_NAME} ${CONTEXT}
   docker push ${SHA_DOCKER_NAME}
   echo ::set-output name=snapshot-tag::"${SNAPSHOT_TAG}"
