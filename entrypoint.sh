e1750b5fb732a991e3a2780171cc8e9b73f0ae35
Dmitry <dmitry.parkhomenko@motorsport.com>
2019-12-30 15:05:38 +0200

    sd

--- entrypoint.sh
+++ entrypoint.sh
@@ -128,10 +128,10 @@ function pushWithSnapshot() {
   local LAST_TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g" | sed -e "s/refs\/tags\///g")
   local LAST_VER=$(git describe --tags $(git rev-list --tags --max-count=1))
   local COMMITS_AHEAD=$(git rev-list ${LAST_VER}.. --count)
-  local SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}.${COMMITS_AHEAD}" 
   if ${LAST_TAG}=${LAST_VER}; then
     local SNAPSHOT_TAG="${LAST_TAG}"
   fi;
+  local SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}.${COMMITS_AHEAD}" 
   if ${COMMITS_AHEAD}=0; then
     local SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}"
   fi;
