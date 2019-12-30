19456a8f438b6e9e47c51745cd7414a6b04385d0
Dmitry <dmitry.parkhomenko@motorsport.com>
2019-12-30 08:59:32 +0200

    try ver

--- entrypoint.sh
+++ entrypoint.sh
@@ -132,8 +132,7 @@ function pushWithSnapshot() {
   local SHA_DOCKER_NAME="${INPUT_NAME}:${SNAPSHOT_TAG}"
   if COMMITS_AHEAD==0; then
     SNAPSHOT_TAG="${LAST_TAG}"
-  fi
-  if LAST_TAG==LAST_VER; then
+  elif ${LAST_TAG}==${LAST_VER}; then
     SNAPSHOT_TAG="${LAST_VER}.${COMMITS_AHEAD}"
   fi
   ##elif LAST_TAG==develop; then
