aedf11410e8d64199908908ddf5d5a6597aa8bb0
Dmitry <dmitry.parkhomenko@motorsport.com>
2019-12-30 16:44:09 +0200

    asd

--- entrypoint.sh
+++ entrypoint.sh
@@ -133,12 +133,12 @@ function pushWithSnapshot() {
   if COMMITS_AHEAD==0; then
     docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} -t ${DOCKERNAME} -t ${INPUT_NAME}:${IMAGE_TAG1} ${CONTEXT}
     docker push ${INPUT_NAME}:${IMAGE_TAG1}
-    elif ${LAST_TAG}=${LAST_VER}; then
-      docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} -t ${DOCKERNAME} -t ${INPUT_NAME}:${IMAGE_TAG1} ${CONTEXT}
-      docker push ${INPUT_NAME}:${IMAGE_TAG1}
-    else
-      docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} -t ${INPUT_NAME}:${IMAGE_TAG2} ${CONTEXT}
-      docker push ${INPUT_NAME}:${IMAGE_TAG2}
+  elif LAST_TAG=${LAST_VER}; then
+    docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} -t ${DOCKERNAME} -t ${INPUT_NAME}:${IMAGE_TAG1} ${CONTEXT}
+    docker push ${INPUT_NAME}:${IMAGE_TAG1}
+  else
+    docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} -t ${INPUT_NAME}:${IMAGE_TAG2} ${CONTEXT}
+    docker push ${INPUT_NAME}:${IMAGE_TAG2}
     fi
   ##echo ::set-output name=snapshot-tag::"${SNAPSHOT_TAG}"
 }
