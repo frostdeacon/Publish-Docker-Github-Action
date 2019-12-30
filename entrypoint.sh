d5d910146d88b2d7ae4fd9727947cd602ce5c596
Dmitry <dmitry.parkhomenko@motorsport.com>
2019-12-30 12:36:50 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -126,18 +126,16 @@ function usesBoolean() {
 
 function pushWithSnapshot() {
   local LAST_TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g" | sed -e "s/refs\/tags\///g")
+  local LAST_VER=$(git describe --tags $(git rev-list --tags --max-count=1))
+  local COMMITS_AHEAD=$(git rev-list ${LAST_VER}.. --count)
+  local SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}.${COMMITS_AHEAD}"
+  local SHA_DOCKER_NAME="${INPUT_NAME}:${SNAPSHOT_TAG}" 
   if ${LAST_TAG}=${LAST_VER}; then
     SNAPSHOT_TAG="${LAST_VER}"
   fi;
-  local LAST_VER=$(git describe --tags $(git rev-list --tags --max-count=1))
-  local COMMITS_AHEAD=$(git rev-list ${LAST_VER}.. --count)
   if ${COMMITS_AHEAD}=0; then
     SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}"
-  elif ${COMMITS_AHEAD}=""; then
-    SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}.${COMMITS_AHEAD}"  
   fi;
-  ##local SNAPSHOT_TAG="${LAST_TAG}-${LAST_VER}.${COMMITS_AHEAD}"
-  local SHA_DOCKER_NAME="${INPUT_NAME}:${SNAPSHOT_TAG}" 
   docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} -t ${DOCKERNAME} -t ${SHA_DOCKER_NAME} ${CONTEXT}
   docker push ${SHA_DOCKER_NAME}
   echo ::set-output name=snapshot-tag::"${SNAPSHOT_TAG}"
